133a7cab43f71fe3e921bc667a90f0f6976e981b
tann <tann1234@mail.ru>
2021-12-12 14:54:25 +0300

    change link

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,9 +1,16 @@
-
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
     def __init__(self, browser, url):
         self.browser = browser
-        # url = "http://selenium1py.pythonanywhere.com/"
         self.url = url
+        self.browser.implicitly_wait(5)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+        return True
 
     def open(self, browser):
         self.browser.get(self.url)
--- test_main_page.py
+++ test_main_page.py
@@ -5,3 +5,9 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # browser, инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open(browser)               # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open(browser)
+    page.should_be_login_link()
